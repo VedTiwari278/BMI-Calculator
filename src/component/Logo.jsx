70e99981cb9aecaec0bd52286eb5c34ed2b3d73c
Ved Prakash Tiwari <148948448+VedTiwari278@users.noreply.github.com>
2024-12-13 17:54:45 +0530

    Commit

--- src/component/Logo.jsx
+++ src/component/Logo.jsx
@@ -2,7 +2,7 @@ function Logo() {
   return (
     <>
       <img
-        src="meter.jpg"
+        src="https://www.bhartiaxa.com/sites/default/files/2023-02/bmi-scale.svg"
         alt="Logo" height="300px"
         className="Image"
       />
